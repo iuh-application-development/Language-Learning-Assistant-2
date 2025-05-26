de980d79633cb00227a590c81238b67f582a3906
PlusNguyn <tranthainguyenpntlop12c8@gmail.com>
2025-05-26 22:02:59 +0700

    change db to posgresql

--- language_assistant/settings.py
+++ language_assistant/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,8 +84,16 @@ WSGI_APPLICATION = 'language_assistant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'SERVICE_URL': config('DB_SERVICE_URL'), 
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'), 
+        'OPTIONS': {
+            'sslmode': config('DB_SSL_MODE'), 
+        },
     }
 }
 
