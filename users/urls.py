3235ac266a7ee69b172bd531af5ea537b1ae8a4b
chien24 <phancongchien8278@gmail.com>
2025-05-27 13:13:03 +0700

    complete forget password by firebase

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login_view, register, logout_view, profile, profile_edit
+from .views import login_view, register, logout_view, profile, profile_edit, forgot_password
 from django.conf import settings
 from django.conf.urls.static import static  
 app_name ='users'
@@ -10,6 +10,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('profile/', profile, name='profile'),
     path('profile/edit/', profile_edit, name='profile_edit'),
+    path('forgot_password/', forgot_password, name='forgot_password'),
 ]
 
 # Thêm cấu hình media trong chế độ debug
